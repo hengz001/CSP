1a50682f91ab1aefe37acdc12ebdef378b53ef30
MSI <MSI@MSI-PC1>
2018-07-12 14:56:16 +0800

    180712 MD5

---
+++ hello/inc/md5.h
@@ -0,0 +1,14 @@
+/*
+ * md5.h
+ *
+ *  Created on: 2018Äê7ÔÂ12ÈÕ
+ *      Author: MSI
+ */
+
+#ifndef __MD5_H__
+#define __MD5_H__
+#include <openssl/md5.h>
+
+int md5(unsigned char *from, int flen, unsigned char *to);
+
+#endif
